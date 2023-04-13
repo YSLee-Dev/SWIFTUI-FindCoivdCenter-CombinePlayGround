09cbc57de76e4684cdf675146196adf68cf4c18c
YoonSuLee <ysleedev01@gmail.com>
2023-04-13 19:16:15 +0900

    Initial Commit

---
+++ SWIFTUI_FindCovidCenter/SWIFTUI_FindCovidCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_FindCovidCenterApp.swift
+//  SWIFTUI_FindCovidCenter
+//
+//  Created by 이윤수 on 2023/04/13.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_FindCovidCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
